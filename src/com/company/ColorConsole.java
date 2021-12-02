a3190810a16954b93ec73a0f28175138e8baf9ce
Sergisa <phebra@yandex.ru>
2021-12-03 01:22:17 +0300

    Этап 3 Перечисления. Модификация Теперь элемент перечисления подразумевает под собой конкретный набор символов

--- src/com/company/ColorConsole.java
+++ src/com/company/ColorConsole.java
@@ -1,28 +1,15 @@
 package com.company;
 
 public class ColorConsole {
+    public static final String RESET = "\u001B[0m";
+
     public static void println(String s) {
         println(s, Color.WHITE);
     }
 
-    public static final String RESET = "";
-
     public static void print(String string, Color color) {
-        switch (color) {
-            case WHITE -> System.out.print("\u001B[37m");
-            case RED -> System.out.print("\u001B[31m");
-            case CYAN -> System.out.print("\u001B[36m");
-            case GREEN -> System.out.print("\u001B[32m");
-            case YELLOW -> System.out.print("\u001B[33m");
-            case BLUE -> System.out.print("\u001B[34m");
-            case PURPLE -> System.out.print("\u001B[35m");
-            case BLACK -> System.out.print("\u001B[30m");
-            default -> {
-                System.out.println();
-            }
-        }
+        System.out.print(color.colorCodeString);
         System.out.print(string);
-        System.out.print("\u001B[0m");
     }
 
     public static void println(String string, Color color) {
@@ -31,13 +18,19 @@ public class ColorConsole {
     }
 
     enum Color {
-        BLACK,
-        RED,
-        GREEN,
-        YELLOW,
-        BLUE,
-        PURPLE,
-        CYAN,
-        WHITE
+        BLACK("\u001B[30m"),
+        BLUE("\u001B[34m"),
+        CYAN("\u001B[36m"),
+        GREEN("\u001B[32m"),
+        PURPLE("\u001B[35m"),
+        RED("\u001B[31m"),
+        WHITE("\u001B[37m"),
+        YELLOW("\u001B[33m");
+        
+        private final String colorCodeString;
+
+        Color(String colorCode) {
+            colorCodeString = colorCode;
+        }
     }
 }
