658d4fed99ae21bbe20308fa333e533fc001de40
Sergisa <phebra@yandex.ru>
2021-12-05 20:46:41 +0300

    Этап 4 Цвета. Оформление теперь полностью настраиваемое Теперь цвет можно указать любой. Использовать можно даже объект класса java.awt.Color Добавлена большая библиотека цветов.

--- src/com/company/ColorConsole.java
+++ src/com/company/ColorConsole.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.awt.*;
+
 public class ColorConsole {
     public static final String RESET = "\u001B[0m";
 
@@ -8,7 +10,7 @@ public class ColorConsole {
     }
 
     public static void print(String string, Color color) {
-        System.out.print(color.colorCodeString);
+        System.out.print(getColorCodeForColor(color));
         System.out.print(string);
     }
 
@@ -17,20 +19,34 @@ public class ColorConsole {
         System.out.println();
     }
 
-    enum Color {
-        BLACK("\u001B[30m"),
-        BLUE("\u001B[34m"),
-        CYAN("\u001B[36m"),
-        GREEN("\u001B[32m"),
-        PURPLE("\u001B[35m"),
-        RED("\u001B[31m"),
-        WHITE("\u001B[37m"),
-        YELLOW("\u001B[33m");
-        
-        private final String colorCodeString;
+    private static String getColorCodeForColor(Color color) {
+        return getColorCodeForColor(null, color, false, false, false, false);
+    }
 
-        Color(String colorCode) {
-            colorCodeString = colorCode;
+    private static String getColorCodeForColor(Color backgroundColor, Color fontColor, boolean isBold, boolean isUnderlined, boolean isItalic, boolean isInversedColor) {
+        StringBuilder colorCodeString = new StringBuilder("\033[");
+        int r = 0, g = 0, b = 0;
+        if (isBold) colorCodeString.append("01;");
+        if (isItalic) colorCodeString.append("03;");
+        if (isUnderlined) colorCodeString.append("04;");
+        if (isInversedColor) colorCodeString.append("07;");
+        if (fontColor != null) colorCodeString.append("38;05;").append(getCodeForColor(fontColor));
+        if (backgroundColor != null) colorCodeString.append("48;05;").append(getCodeForColor(backgroundColor));
+        return colorCodeString.append("m").toString();
+    }
+
+    private static int getCodeForColor(Color color) {
+        float r, g, b;
+        r = color.getRed();
+        g = color.getGreen();
+        b = color.getBlue();
+        if (r == g && r == b) {
+            return (int) ((r > 239) ? 15 : Math.floor(r / 10) + 232);
         }
+        r = (int) (Math.floor(r / 51));
+        g = (int) Math.floor(g / 51);
+        b = (int) Math.floor(b / 51);
+
+        return (int) (16 + 36 * r + 6 * g + b);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,14 @@
 package com.company;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        ColorConsole.println("Hello World", ColorConsole.Color.RED);
-        ColorConsole.println("Hello World", ColorConsole.Color.CYAN);
-        ColorConsole.println("Hello World", ColorConsole.Color.PURPLE);
-        ColorConsole.print("Hello World  ", ColorConsole.Color.GREEN);
-        ColorConsole.print("Hello World", ColorConsole.Color.YELLOW);
+        ColorConsole.println("Hello World", Color.RED);
+        ColorConsole.println("Hello World", Color.CYAN);
+        ColorConsole.println("Hello World", Color.GREEN);
+        ColorConsole.println("Hello World", Color.YELLOW);
+        ColorConsole.println("Hello World", MaterialColors.MYRTLE_GREEN);
     }
 }
